957fdf1b3345cb4961dbc7d8bbf82f9af4db2b8e
vdegenne <vdegenne@gmail.com>
2019-04-18 01:56:42 +0200

    take care of smalls and doubles

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const smalls = [
   /* hiragana */
   ['ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ'],
   ['ゃ', 'ゅ', 'ょ'],
-  ['っ'],
   /* katakana */
   ['ァ', 'ィ', 'ゥ', 'ェ', 'ォ'],
-  ['ャ', 'ュ', 'ョ'],
-  ['ッ']
+  ['ャ', 'ュ', 'ョ']
 ].reduce((a, b) => a.concat(b))
+
+const doubles = ['っ', 'ッ']
 
 const addSpaces = input => {
   let spaced = ''
@@ -23,14 +23,15 @@ const addSpaces = input => {
     if (
       isJapanese(input[i]) &&
       isJapanese(input[i + 1]) &&
-      !smalls.includes(input[i])
+      !smalls.includes(input[i + 1]) &&
+      !doubles.includes(input[i])
     ) {
       spaced += ' '
     }
   }
 
   /* take care of the dot double consonent separator */
-  spaced = spaced.replace(/(っ|ッ)\../g, m => {
+  spaced = spaced.replace(new RegExp(`(${doubles.join('|')})\..`, 'g'), m => {
     const roman = hepburn.fromKana(m.replace('.', '')).split('')
     roman.splice(1, 0, '.')
     return roman.join('')
--- test.mjs
+++ test.mjs
@@ -1,7 +1,5 @@
 import romanize from '.'
 
-console.log(romanize.fromKana('いっ.ぴき', true))
-process.exit()
 console.log(
   `みみ.が (beautify = false) === 'MIMI.GA' [${romanize.fromKana('みみ.が') ===
     'MIMI.GA'}]`
