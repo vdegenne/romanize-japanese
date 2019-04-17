3025048b2f2189f0d6b384433f128efdbb92b5f9
vdegenne <vdegenne@gmail.com>
2019-04-17 19:53:35 +0200

    add smalls

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const hepburn = require('hepburn')
 const japaneseRegExp = /([\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf])+/
 const isJapanese = input => input.match(japaneseRegExp)
 
+const smalls = [
+  /* hiragana */
+  ['ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ'],
+  ['ゃ', 'ゅ', 'ょ'],
+  ['っ'],
+  /* katakana */
+  ['ァ', 'ィ', 'ゥ', 'ェ', 'ォ'],
+  ['ャ', 'ュ', 'ョ'],
+  ['ッ']
+].reduce((a, b) => a.concat(b))
+
 const addSpaces = input => {
   let spaced = ''
   for (let i = 0; i < input.length; ++i) {
@@ -12,7 +23,7 @@ const addSpaces = input => {
     if (
       isJapanese(input[i]) &&
       isJapanese(input[i + 1]) &&
-      !['ゃ', 'ゅ', 'ょ', '　'].includes(input[i + 1])
+      !smalls.includes(input[i + 1])
     ) {
       spaced += ' '
     }
